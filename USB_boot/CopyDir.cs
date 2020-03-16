f3122578770e25b1090c0a7cace0e2bbb0ab12d4
Petr-Senfeld <59889161+Petr-Senfeld@users.noreply.github.com>
2020-03-17 00:35:16 +0100

    First commit
    Almost done

---
+++ USB_boot/CopyDir.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace USB_boot
+{
+    class CopyDir
+    {
+        static public void CopyFolder(string sourceFolder, string destFolder)
+        {
+            if (!Directory.Exists(destFolder))
+                Directory.CreateDirectory(destFolder);
+            string[] files = Directory.GetFiles(sourceFolder);
+            foreach (string file in files)
+            {
+                string name = Path.GetFileName(file);
+                string dest = Path.Combine(destFolder, name);
+                File.Copy(file, dest);
+            }
+            string[] folders = Directory.GetDirectories(sourceFolder);
+            foreach (string folder in folders)
+            {
+                string name = Path.GetFileName(folder);
+                string dest = Path.Combine(destFolder, name);
+                CopyFolder(folder, dest);
+            }
+        }
+    }
+}
---
+++ USB_boot/Format.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Management;
+using System.Text;
+using System.Linq;
+using System.IO;
+using System.Diagnostics;
+
+namespace USB_boot
+{
+    class Format
+    {
+        public static bool FormatDrive(string driveLetter, string label = "Windows10", string fileSystem = "FAT32", bool quickFormat = true, bool enableCompression = false, int? clusterSize = null)
+        {
+            bool success = false;
+            string drive = driveLetter + ":";
+            try
+            {
+                var di = new DriveInfo(drive);
+                var psi = new ProcessStartInfo();
+                psi.FileName = "format.com";
+                psi.CreateNoWindow = true; //if you want to hide the window
+                psi.WorkingDirectory = Environment.SystemDirectory;
+                // following commands can be adjusted to expected need
+                // **************************************************
+                psi.Arguments = "/FS:" + fileSystem +
+                                             " /Y" +
+                                             " /V:" + label +
+                                             (quickFormat ? " /Q" : "") +
+                                             ((fileSystem == "FAT32" && enableCompression) ? " /C" : "") +
+                                             (clusterSize.HasValue ? " /A:" + clusterSize.Value : "") +
+                                             " " + drive;
+                // **************************************************
+                psi.UseShellExecute = false;
+                psi.CreateNoWindow = true;
+                psi.RedirectStandardOutput = true;
+                psi.RedirectStandardInput = true;
+                var formatProcess = Process.Start(psi);
+                var swStandardInput = formatProcess.StandardInput;
+                swStandardInput.WriteLine();
+                formatProcess.WaitForExit();
+                success = true;
+            }
+            catch (Exception) { }
+            return success;
+        }
+    }
+}
